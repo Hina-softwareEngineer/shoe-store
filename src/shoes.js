08691742299d736a8e2f8ce0524a5cc8b0a8540e
HINA KHADIM <hinakhadim2002@gmail.com>
2020-07-06 22:59:37 +0500

    collecting data

---
+++ src/shoes.js
@@ -0,0 +1,57 @@
+const shoes = {
+  "0": {
+    name: "VALOUR BLUE",
+    img:
+      "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  "1": {
+    name: "JORDAN MARS 270 LONDON",
+    img:
+      "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  "2": {
+    name: "RACER BLUE",
+    img:
+      "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  "3": {
+    name: "Red Nike Sneaker",
+    img:
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  "4": {
+    name: "Maroon Plimsool",
+    img:
+      "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  "5": {
+    name: "PAIR OF WHITE SNEAKERS",
+    img:
+      "https://images.unsplash.com/photo-1544441892-794166f1e3be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  "6": {
+    name: "BLACK PUMA SUEDE",
+    img:
+      "https://images.unsplash.com/photo-1545289414-1c3cb1c06238?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  "7": {
+    name: "WHITE AND BLACK NIKE AIR FORCE",
+    img:
+      "https://images.unsplash.com/photo-1587563871167-1ee9c731aefb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  "8": {
+    name: "HIGH TOP SHOES",
+    img:
+      "https://images.unsplash.com/photo-1556048219-bb6978360b84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  "9": {
+    name: "WHITE-GREEN SNEAKERS",
+    img:
+      "https://media.istockphoto.com/photos/modern-sport-shoes-picture-id623270836?b=1&k=6&m=623270836&s=612x612&h=KcNuCJ2GEqpYCcVRNOOk3Atsm_O9VZzzzmNIztfFYag=",
+  },
+  "10": {
+    name: "BLUE AIR JORDAN",
+    img:
+      "https://images.unsplash.com/photo-1578116922645-3976907a7671?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+};
